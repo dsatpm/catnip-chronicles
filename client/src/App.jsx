2956b22aec9a61de711144eca8d1f0fc38c454fe
dsatpm <jeremiahjh@gmail.com>
2024-01-02 21:18:50 -0800

    file structure, initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<>
+			<section>
+				App!!!
+			</section>
+		</>
+	);
+};
+
+export default App;
