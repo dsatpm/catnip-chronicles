81f0d772f24f1151c69ad2bd1649b698af19f95a
dsatpm <jeremiahjh@gmail.com>
2024-01-08 19:44:26 -0800

    router fixed and working!!!

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,7 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './signup.scss';
+
 const Signup = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log(username, password);
+		try {
+			const res = await axios.post('http://localhost:5000/auth/signup', {
+				username,
+				password,
+			});
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 	return (
 		<>
-			<div className='signup'>Signup</div>
+			<section className='signup'>
+				<form onSubmit={handleSubmit}>
+					<h2>Signup</h2>
+					<input
+						type='username'
+						placeholder='Create Username'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						required
+					/>
+					<input
+						type='password'
+						placeholder='Create Password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+					<button type='submit'>
+						<span>Signup</span>
+						</button>
+						<p>
+							Already have an account? <Link to='/login'>Login</Link>
+						</p>
+				</form>
+			</section>
 		</>
 	);
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import Homepage from './components/Homepage/Homepage';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <h1>Wrong page!</h1>,
+		children: [
+			{
+				index: true,
+				element: <Homepage/>
+			}, {
+				path: '/signup',
+				element: <Signup />
+			},
+			{
+					path: '/login',
+					element: <Login />
+			}
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<RouterProvider router={router} />
 );
