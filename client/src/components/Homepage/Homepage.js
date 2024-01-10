5fc7cdb6b8946c53d1130a0ab8d86d3ab3628b20
dsatpm <jeremiahjh@gmail.com>
2024-01-09 19:37:31 -0800

    install option front end stuff, file cleanup

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,5 +1,8 @@
 import './homepage.scss';
-import { Link } from 'react-router-dom';
+import { Link } from'react-router-dom';
+
+
+
 
 const Homepage = () => {
 	return (
@@ -8,8 +11,9 @@ const Homepage = () => {
 				<div className='text-container'>
 					<h1>Catnip Chronicles</h1>
 					<div className='nav-container'>
-						<Link to='/login'>Login</Link>
-						<Link to='/signup'>Signup</Link>
+					<Link to='/login' className='nav-link'>Login</Link>
+					<Link to='/signup' className='nav-link'>Signup</Link>
+					<Link id='buttonInstall' className='nav-link'>Install</Link>
 					</div>
 				</div>
 			</section>
