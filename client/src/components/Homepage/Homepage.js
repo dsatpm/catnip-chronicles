c6f407963f0ee692f485587926ade697f8971054
dsatpm <jeremiahjh@gmail.com>
2024-01-09 21:51:52 -0800

    push

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -14,12 +14,20 @@ const Homepage = () => {
 						<h1>Welcome!</h1>
 						<h2>
 							To the best pixelated 2D platformer ever made in 2
-							weeks (or less) by a small group of really cool, really hire-able junior developers.
+							weeks (or less) by a small group of really cool,
+							really hire-able junior developers.
 						</h2>
 						<p>
 							We used React, Node, Express, and MongoDb (MERN) to
 							create this hecka fun game. We sincerely hope you
-							find it charming and enjoyable! Click one of the buttons below to get yourself started. Obviously, if you've never been here before, go ahead and sign up for a new account (it's FREE!) and start playing! If you love the game so much, you can even download it to play on your local machine, or if you ever lose internet service or whatever. We're not your mom, we're not your dad. Live your own life and have fun!
+							find it charming and enjoyable! Click one of the
+							buttons below to get yourself started. Obviously, if
+							you've never been here before, go ahead and sign up
+							for a new account (it's FREE!) and start playing! If
+							you love the game so much, you can even download it
+							to play on your local machine, or if you ever lose
+							internet service or whatever. We're not your mom,
+							we're not your dad. Live your own life and have fun!
 						</p>
 					</div>
 					<div className='nav-container'>
