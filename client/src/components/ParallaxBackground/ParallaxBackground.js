da3a1eda84f926b74bc3c428d8810bc39677540d
dsatpm <jeremiahjh@gmail.com>
2024-01-09 19:42:06 -0800

    new stuff new stuff

---
+++ client/src/components/ParallaxBackground/ParallaxBackground.js
@@ -0,0 +1,11 @@
+import './parallax-bg.scss';
+
+const ParallaxBackground = () => {
+  return (
+    <>
+    <div className='parallax-bg'></div>
+    </>
+  )
+}
+
+export default ParallaxBackground
