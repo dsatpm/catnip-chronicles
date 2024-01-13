78caf5973d6fb69fb3c4d3fa57f8e44baa209c23
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-12 20:08:22 -0800

    added styling

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 // set up Express app
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
